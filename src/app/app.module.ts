7f9846698e2e7f5848a4282f35bd7b4c2fc07e43
shyam <radheshyam.srinivasan@gmail.com>
2021-02-07 19:51:15 +0800

    get cat breed information

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CatmenuComponent } from './catmenu/catmenu.component';
+import { CatService } from './cat.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +17,10 @@ import { CatmenuComponent } from './catmenu/catmenu.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CatService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/catmenu/catmenu.component.ts
+++ src/app/catmenu/catmenu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CatService } from '../cat.service';
 
 @Component({
   selector: 'app-catmenu',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatmenuComponent implements OnInit {
 
-  constructor() { }
+  catBreeds:any;
+  constructor(private service:CatService) {
+    
+   }
 
   ngOnInit(): void {
+    this.service.getCatBreeds().subscribe(
+      (responseData)=>{
+          console.log(responseData);
+          this.catBreeds=responseData;
+      }
+    );
   }
 
 }
