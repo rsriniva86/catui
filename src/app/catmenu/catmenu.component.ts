8bec1bb14fc7c2e163490f4cee94163e6741e72e
shyam <radheshyam.srinivasan@gmail.com>
2021-02-08 06:10:41 +0800

    updated catmenu component to show side menu and main item

--- src/app/catmenu/catmenu.component.ts
+++ src/app/catmenu/catmenu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CatService } from '../cat.service';
+import { cat } from './cat';
 
 @Component({
   selector: 'app-catmenu',
@@ -9,17 +10,26 @@ import { CatService } from '../cat.service';
 export class CatmenuComponent implements OnInit {
 
   catBreeds:any;
-  constructor(private service:CatService) {
-    
+  isBreedSelected:boolean;
+  selectedBreed:cat[]=[];
+  constructor(private service:CatService) {  
+    this.isBreedSelected=false;  
    }
 
   ngOnInit(): void {
     this.service.getCatBreeds().subscribe(
       (responseData)=>{
-          console.log(responseData);
+          //console.log(responseData);
           this.catBreeds=responseData;
       }
     );
+  }
+
+  public selectBreed(breed:cat):void{
+
+    this.selectedBreed=[breed];
+    console.log(this.selectedBreed[0].name);
+    this.isBreedSelected=true;
   }
 
 }
